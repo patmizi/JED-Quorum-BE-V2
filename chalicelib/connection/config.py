9f45970beceef519f8b959030bf580bc7ce839c9
Patrick Miziewicz <patrick.miziewicz@gmail.com>
2018-10-10 17:19:52 +1100

    Pipeline update

--- chalicelib/connection/config.py
+++ chalicelib/connection/config.py
@@ -1,8 +1,8 @@
-#import os
+import os
 
-#from definitions import ROOT_DIR, TEST_DB
+from definitions import ROOT_DIR, TEST_DB
 
 connection_strings = {
     "prod": "mysql+pymysql://root:password123==@quorum-db-instance.cmozvfqehajr.ap-southeast-2.rds.amazonaws.com:3306/quorumdb",
-    # "test": 'sqlite:///' + os.path.join(ROOT_DIR, 'database', TEST_DB)
+    "test": 'sqlite:///' + os.path.join(ROOT_DIR, 'database', TEST_DB)
 }
