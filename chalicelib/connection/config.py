17dff377d15a59006e7dc34692c05302821d3caf
Patrick Miziewicz <patrick.miziewicz@gmail.com>
2018-10-17 18:16:59 +1100

    Remove creds. They don't point to anything now anyways

--- chalicelib/connection/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from definitions import ROOT_DIR, TEST_DB
-
-connection_strings = {
-    "prod": "mysql+pymysql://root:password123==@myquorum.cmozvfqehajr.ap-southeast-2.rds.amazonaws.com:3306/quorumdb",
-    "test": 'sqlite:///' + os.path.join(ROOT_DIR, 'database', TEST_DB)
-}
-
--- chalicelib/connection/prod_config.py
+++
@@ -1,3 +0,0 @@
-connection_strings = {
-    "prod": "mysql+pymysql://root:password123==@myquorum.cmozvfqehajr.ap-southeast-2.rds.amazonaws.com:3306/quorumdb"
-}
